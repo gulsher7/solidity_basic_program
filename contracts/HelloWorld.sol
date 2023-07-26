d9378fa3ac90ea1a56a4d31dd9154204d28fb801
Gulsher Khan <gulsherkhan@Gulshers-MacBook-Air.local>
2023-07-26 15:45:10 +0530

    learned operators it's tottaly similar to other programming language' clear

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,12 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+
 contract HelloWorld {
+  event ValueLogged(string message);
   string message;
   constructor() public {
      message = "Hello World";
   }
+
+  
   function sayhello() view public returns (string memory){
+     
             return message;
   }
 }
